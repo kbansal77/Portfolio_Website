1d3f588418495ec1244c913d488bd7705092a837
Kartik Bansal <kartikbansal2000@gmail.com>
2022-02-02 16:23:45 +0530

    new portfolio

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    var width = window.innerWidth;
+    if(width<=500){
+      console.log(typeof width)
+      document.getElementById("mySidenav").style.width = "65vw";
+    }
+    else{
+      document.getElementById("mySidenav").style.width = "25vw";
+    }
   }
   
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-
-const checkbox = document.getElementById("checkbox");
-
-checkbox.addEventListener('change', ()=>{
-  document.body.classList.toggle('dark');
-})
